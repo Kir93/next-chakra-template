ff865c96a118079afac077f42eb4af8db4265fcb
Kir93 <kir931028@gmail.com>
2023-07-14 16:05:45 +0900

    update apply new dummy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,13 @@
 'use client';
 
+import AutoHeightImg from '@/components/AutoHeightImg';
 import { Heading, VStack } from '@chakra-ui/react';
-import Img from '@/components/Img';
-import Logo from '@/public/next.svg';
 
 export default function Home() {
   return (
-    <VStack
-      spacing="30px"
-      minH="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      px="30px"
-    >
+    <VStack spacing="30px" w="100%" minH="100vh" display="flex" px="30px">
       <Heading>Next Chakra Template</Heading>
-      <Img src={Logo} alt="Next.js Logo" objectFit="cover" w="150px" />
+      <AutoHeightImg src="/vercel.jpeg" alt="test image" />
     </VStack>
   );
 }
